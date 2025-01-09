583ce87c606ed8d92b8cd9f1b29a680a120f78d9
Jami Karimkulov <jami.karimkul@gmail.com>
2025-01-09 11:40:24 -0500

    Add asynchronous javaScript: async/await

--- 02-JS-review /script.js
+++ 02-JS-review /script.js
@@ -286,7 +286,21 @@ const booksAfterUpdate = booksAfterDelete.map((book) =>
 booksAfterUpdate;
 */
 // Asynchronous JavaScript: Promises
-fetch("https://jsonplaceholder.typicode.com/todos")
-    .then((res) => res.json())
-    .then((data) => console.log(data));
+// fetch("https://jsonplaceholder.typicode.com/todos")
+//     .then((res) => res.json())
+//     .then((data) => console.log(data));
 // console.log("jonas");
+
+// Asynchronous JavaScript: Async/Await
+async function getTodos() {
+    const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+    const data = await res.json();
+    console.log(data);
+    return data;
+}
+
+const todos = getTodos();
+
+console.log(todos);
+
+console.log("jonas");
