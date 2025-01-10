6b2717fb86063bb02a8ee63c3d2b4c20bc94e331
Jami Karimkulov <jami.karimkul@gmail.com>
2025-01-10 12:46:41 -0500

    Add react fragments

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -73,12 +73,20 @@ function Menu() {
     return (
         <main className="menu">
             <h2>Our Menu</h2>
+
             {numPizza > 0 ? (
-                <ul className="pizzas">
-                    {pizzas.map((pizza) => (
-                        <Pizza pizzaObj={pizza} key={pizza.name} />
-                    ))}
-                </ul>
+                <>
+                    <p>
+                        Authentic Italian causine. 6 creative dishes to choose
+                        from. All from our stone even, all organic, all
+                        delicious.
+                    </p>
+                    <ul className="pizzas">
+                        {pizzas.map((pizza) => (
+                            <Pizza pizzaObj={pizza} key={pizza.name} />
+                        ))}
+                    </ul>
+                </>
             ) : (
                 <p>We're still working on menu. Please come back later!</p>
             )}
