233330f2104347bc89573c40aa470dcf2eca56e1
Jami Karimkulov <jami.karimkul@gmail.com>
2025-01-10 12:11:32 -0500

    Add conditional rendering with ternaries

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -73,12 +73,14 @@ function Menu() {
     return (
         <main className="menu">
             <h2>Our Menu</h2>
-            {numPizza > 0 && (
+            {numPizza > 0 ? (
                 <ul className="pizzas">
                     {pizzas.map((pizza) => (
                         <Pizza pizzaObj={pizza} key={pizza.name} />
                     ))}
                 </ul>
+            ) : (
+                <p>We're still working on menu. Please come back later!</p>
             )}
 
             {/* <Pizza
@@ -113,7 +115,7 @@ function Pizza(props) {
 
 function Footer() {
     const hour = new Date().getHours();
-    const openHour = 11;
+    const openHour = 12;
     const closeHour = 22;
     const isOpen = hour >= openHour && hour <= closeHour;
     console.log(isOpen);
@@ -122,7 +124,7 @@ function Footer() {
     // else alert("Sorry we are closed");
     return (
         <footer className="footer">
-            {isOpen && (
+            {isOpen ? (
                 <div className="order">
                     <p>
                         We're open until {closeHour}:00. Come visit us or order
@@ -130,6 +132,11 @@ function Footer() {
                     </p>
                     <button className="btn">Order</button>
                 </div>
+            ) : (
+                <p>
+                    We're happy to welcome you between {openHour}:00 and{" "}
+                    {closeHour}:00
+                </p>
             )}
         </footer>
     );
