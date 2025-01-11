9880c7c532d81098bdd8d9e55ab6027e61d1a0a4
Jami Karimkulov <jami.karimkul@gmail.com>
2025-01-11 13:08:37 -0500

    Create a state variable with useState

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const messages = [
     "Learn React 🧑‍💻",
     "Apply for jobs 💼",
@@ -5,19 +7,20 @@ const messages = [
 ];
 
 export default function App() {
-    const step = 1;
+    const [step, setStep] = useState(1);
+
     function handlePrevious() {
-        alert("Previous");
+        if (step > 1) setStep(step - 1);
     }
     function handleNext() {
-        alert("Next");
+        if (step < 3) setStep(step + 1);
     }
     return (
         <div className="steps">
             <div className="numbers">
-                <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-                <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-                <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+                <div className={step >= 1 ? "active" : ""}>1</div>
+                <div className={step >= 2 ? "active" : ""}>2</div>
+                <div className={step >= 3 ? "active" : ""}>3</div>
             </div>
             <p className="message">
                 STEP {step}: {messages[step - 1]}
@@ -31,7 +34,7 @@ export default function App() {
                 </button>
                 <button
                     style={{ backgroundColor: "#7950f2", color: "#fff" }}
-                    onClick={handlePrevious}
+                    onClick={handleNext}
                 >
                     Next
                 </button>
