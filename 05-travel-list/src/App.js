a546b071bcc61b4b367c7f49c7d10c9e1a187ac3
Jami Karimkulov <jami.karimkul@gmail.com>
2025-01-13 13:04:40 -0500

    Delete an item: more child-to-parent communication

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -3,6 +3,10 @@ import { useState } from "react";
 export default function App() {
     const [items, setItems] = useState([]);
 
+    function handleDeleteItem(id) {
+        setItems((items) => items.filter((item) => item.id !== id));
+    }
+
     function handleAddItems(item) {
         setItems((items) => [...items, item]);
     }
@@ -11,7 +15,11 @@ export default function App() {
         <div className="app">
             <Logo />
             <Form onAddItems={handleAddItems} />
-            <PackingList items={items} setItems={setItems} />
+            <PackingList
+                items={items}
+                setItems={setItems}
+                onDeleteItem={handleDeleteItem}
+            />
             <Stats />
         </div>
     );
@@ -67,25 +75,29 @@ function Form({ onAddItems }) {
     );
 }
 
-function PackingList({ items }) {
+function PackingList({ items, onDeleteItem }) {
     return (
         <div className="list">
             <ul>
                 {items.map((item) => (
-                    <Item item={item} key={item.id} />
+                    <Item
+                        item={item}
+                        key={item.id}
+                        onDeleteItem={onDeleteItem}
+                    />
                 ))}
             </ul>
         </div>
     );
 }
 
-function Item({ item }) {
+function Item({ item, onDeleteItem }) {
     return (
         <li>
             <span style={item.packed ? { textDecoration: "line-through" } : {}}>
                 {item.quantity} {item.description}
             </span>
-            <button>❌</button>
+            <button onClick={() => onDeleteItem(item.id)}>❌</button>
         </li>
     );
 }
