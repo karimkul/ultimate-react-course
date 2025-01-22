849153ab9d6a4f6f67f8c341a8de3b0e9fac104d
Jami Karimkulov <jami.karimkul@gmail.com>
2025-01-21 22:40:44 -0500

    Handle hover events

--- 07-usepopcorn/src/starRating.js
+++ 07-usepopcorn/src/starRating.js
@@ -14,7 +14,8 @@ const textStyle = {
 };
 
 export default function StartRating({ maxRating = 5 }) {
-    const [rating, setRating] = useState(1);
+    const [rating, setRating] = useState(0);
+    const [tempRating, setTempRating] = useState(0);
 
     function handleRating(rating) {
         setRating(rating);
@@ -26,12 +27,16 @@ export default function StartRating({ maxRating = 5 }) {
                 {Array.from({ length: maxRating }, (_, i) => (
                     <Start
                         key={i}
+                        full={
+                            tempRating ? tempRating >= i + 1 : rating >= i + 1
+                        }
                         onRate={() => handleRating(i + 1)}
-                        full={rating >= i + 1}
+                        onHoverIn={() => setTempRating(i + 1)}
+                        onHoverOut={() => setTempRating(0)}
                     />
                 ))}
             </div>
-            <p style={textStyle}>{rating || ""}</p>
+            <p style={textStyle}>{tempRating || rating || ""}</p>
         </div>
     );
 }
@@ -43,9 +48,15 @@ const starStyle = {
     cursor: "pointer"
 };
 
-function Start({ onRate, full }) {
+function Start({ onRate, full, onHoverIn, onHoverOut }) {
     return (
-        <span role="button" style={starStyle} onClick={onRate}>
+        <span
+            role="button"
+            style={starStyle}
+            onClick={onRate}
+            onMouseEnter={onHoverIn}
+            onMouseLeave={onHoverOut}
+        >
             {full ? (
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
