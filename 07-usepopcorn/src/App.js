0ac437b11d57a7273a3ee5e1b316ffff0f987133
Jami Karimkulov <jami.karimkul@gmail.com>
2025-01-25 12:38:24 -0500

    Add handling errors

--- 07-usepopcorn/src/App.js
+++ 07-usepopcorn/src/App.js
@@ -1,48 +1,47 @@
 // import { func } from "prop-types";
-import { func } from "prop-types";
 import { useEffect, useState } from "react";
 
-const tempMovieData = [
-    {
-        imdbID: "tt1375666",
-        Title: "Inception",
-        Year: "2010",
-        Poster: "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg"
-    },
-    {
-        imdbID: "tt0133093",
-        Title: "The Matrix",
-        Year: "1999",
-        Poster: "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg"
-    },
-    {
-        imdbID: "tt6751668",
-        Title: "Parasite",
-        Year: "2019",
-        Poster: "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg"
-    }
-];
+// const tempMovieData = [
+//     {
+//         imdbID: "tt1375666",
+//         Title: "Inception",
+//         Year: "2010",
+//         Poster: "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg"
+//     },
+//     {
+//         imdbID: "tt0133093",
+//         Title: "The Matrix",
+//         Year: "1999",
+//         Poster: "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg"
+//     },
+//     {
+//         imdbID: "tt6751668",
+//         Title: "Parasite",
+//         Year: "2019",
+//         Poster: "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg"
+//     }
+// ];
 
-const tempWatchedData = [
-    {
-        imdbID: "tt1375666",
-        Title: "Inception",
-        Year: "2010",
-        Poster: "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-        runtime: 148,
-        imdbRating: 8.8,
-        userRating: 10
-    },
-    {
-        imdbID: "tt0088763",
-        Title: "Back to the Future",
-        Year: "1985",
-        Poster: "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-        runtime: 116,
-        imdbRating: 8.5,
-        userRating: 9
-    }
-];
+// const tempWatchedData = [
+//     {
+//         imdbID: "tt1375666",
+//         Title: "Inception",
+//         Year: "2010",
+//         Poster: "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+//         runtime: 148,
+//         imdbRating: 8.8,
+//         userRating: 10
+//     },
+//     {
+//         imdbID: "tt0088763",
+//         Title: "Back to the Future",
+//         Year: "1985",
+//         Poster: "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+//         runtime: 116,
+//         imdbRating: 8.5,
+//         userRating: 9
+//     }
+// ];
 
 const average = (arr) =>
     arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
@@ -53,16 +52,35 @@ export default function App() {
     const [movies, setMovies] = useState([]);
     const [watched, setWatched] = useState([]);
     const [isLoading, setIsLodaing] = useState(false);
+    const [error, setError] = useState("");
 
     useEffect(function () {
         async function fetchMovies() {
-            setIsLodaing(true);
-            const res = await fetch(
-                `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
-            );
-            const data = await res.json();
-            setMovies(data.Search);
-            setIsLodaing(false);
+            try {
+                setIsLodaing(true);
+                setError("");
+
+                const res = await fetch(
+                    `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+                );
+
+                if (!res.ok)
+                    throw new Error(
+                        "Something went wrong with fetching Movies"
+                    );
+
+                const data = await res.json();
+
+                if (data.Response === "False")
+                    throw new Error("Movie not found");
+
+                setMovies(data.Search);
+            } catch (err) {
+                console.error(err);
+                setError(err.message);
+            } finally {
+                setIsLodaing(false);
+            }
         }
         fetchMovies();
     }, []);
@@ -76,7 +94,11 @@ export default function App() {
             </NavBar>
             <Main>
                 <Box>
-                    {isLoading ? <Loader /> : <MovieList movies={movies} />}
+                    {/* {isLoading ? <Loader /> : <MovieList movies={movies} />} */}
+
+                    {isLoading && <Loader />}
+                    {!isLoading && !error && <MovieList movies={movies} />}
+                    {error && <ErrorMessage message={error} />}
                 </Box>
                 <Box>
                     <WatchedSummary watched={watched} />
@@ -88,6 +110,13 @@ export default function App() {
 }
 function Loader() {
     return <p className="loader">Loading...</p>;
+}
+function ErrorMessage({ message }) {
+    return (
+        <p className="error">
+            <span>❌</span> {message}
+        </p>
+    );
 }
 
 function NavBar({ children }) {
