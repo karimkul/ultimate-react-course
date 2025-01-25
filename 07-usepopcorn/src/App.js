cdbdc1112d79b45363701b913963340f4b51c15d
Jami Karimkulov <jami.karimkul@gmail.com>
2025-01-25 10:43:04 -0500

    How NOT to fetch data in react

--- 07-usepopcorn/src/App.js
+++ 07-usepopcorn/src/App.js
@@ -45,9 +45,21 @@ const tempWatchedData = [
 const average = (arr) =>
     arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
+const KEY = "8ff8de19";
 export default function App() {
-    const [movies, setMovies] = useState(tempMovieData);
-    const [watched, setWatched] = useState(tempWatchedData);
+    const [movies, setMovies] = useState([]);
+    const [watched, setWatched] = useState([]);
+
+    fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data.Search);
+        })
+        .catch((err) => console.error("Fetch Error:", err));
+
+    // fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=${KEY}&s=interestellar`)
+    //     .then((res) => res.json())
+    //     .then((data) => console.log(data));
 
     return (
         <>
--- 07-usepopcorn/src/index.js
+++ 07-usepopcorn/src/index.js
@@ -1,33 +1,33 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
-import StartRating from "./starRating";
+import "./index.css";
+import App from "./App";
+// import StartRating from "./starRating";
 
-function Star() {
-    const [movieRating, setMovieRating] = useState(0);
-    return (
-        <div>
-            <StartRating
-                color="blue"
-                maxRating={10}
-                onSetRating={setMovieRating}
-            />
-            <p>This movie was rated {movieRating} stars</p>
-        </div>
-    );
-}
+// function Star() {
+//     const [movieRating, setMovieRating] = useState(0);
+//     return (
+//         <div>
+//             <StartRating
+//                 color="blue"
+//                 maxRating={10}
+//                 onSetRating={setMovieRating}
+//             />
+//             <p>This movie was rated {movieRating} stars</p>
+//         </div>
+//     );
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        {/* <App /> */}
-        <StartRating
+        <App />
+        {/* <StartRating
             maxRating={5}
             messages={["Terrible", "Bad", "Okey", "Good", "Amazing"]}
         />
-        <StartRating size={12} color="red" className="test" defaultRating={3} />
+        <StartRating size={12} color="red" className="test" defaultRating={3} /> */}
 
-        <Star />
+        {/* <Star /> */}
     </React.StrictMode>
 );
