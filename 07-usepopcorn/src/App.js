b0f7c468686eadb588eec1cdcd7782d54c76662e
Jami Karimkulov <jami.karimkul@gmail.com>
2025-01-31 13:21:33 -0500

    Initialize state with a callback (lazy initial state)

--- 07-usepopcorn/src/App.js
+++ 07-usepopcorn/src/App.js
@@ -10,23 +10,37 @@ const KEY = "8ff8de19";
 export default function App() {
     const [query, setQuery] = useState("");
     const [movies, setMovies] = useState([]);
-    const [watched, setWatched] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState("");
     const [selectedId, setSelectedId] = useState(null);
+    // const [watched, setWatched] = useState([]);
+    const [watched, setWatched] = useState(function () {
+        const storedValue = localStorage.getItem("watched");
+        return JSON.parse(storedValue);
+    });
 
     function handleSelectMovie(id) {
         setSelectedId((selectedId) => (id === selectedId ? null : id));
     }
+
     function handleCloseMovie() {
         setSelectedId(null);
     }
+
     function handleAddWatched(movie) {
         setWatched((watched) => [...watched, movie]);
+        // localStorage.setItem("watched", JSON.stringify([...watched, movie]));
     }
+
     function handleDeleteWatched(id) {
         setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
     }
+    useEffect(
+        function () {
+            localStorage.setItem("watched", JSON.stringify(watched));
+        },
+        [watched]
+    );
 
     useEffect(
         function () {
