565cae7652104b6648de25294710feb8eb8bdcec
Jami Karimkulov <jami.karimkul@gmail.com>
2025-02-01 10:22:34 -0500

    Add Refs to persist data between renders

--- 07-usepopcorn/src/App.js
+++ 07-usepopcorn/src/App.js
@@ -266,6 +266,16 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     const [movie, setMovie] = useState({});
     const [isLoading, setIsLoading] = useState(false);
     const [userRating, setUserRating] = useState("");
+
+    const countRef = useRef(0);
+
+    useEffect(
+        function () {
+            if (userRating) countRef.current = countRef.current++;
+        },
+        [userRating]
+    );
+
     const isWatched = watched.map((movie) => movie.imdbID).includes(selectedId);
     const watchedUserRating = watched.find(
         (movie) => movie.imdbID === selectedId
@@ -304,7 +314,8 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
             poster,
             imdbRating: Number(imdbRating),
             runtime: Number(runtime.split(" ").at(0)),
-            userRating
+            userRating,
+            countRatingDecisions: countRef.current
         };
         onAddWatched(newWatchedMovie);
         onCloseMovie();
