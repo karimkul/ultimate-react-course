2c7538cbf619fdf698cc00c5ab4646e93c4d2161
Jami Karimkulov <jami.karimkul@gmail.com>
2025-02-02 13:41:52 -0500

    Add the react-quiz app

--- 10-react-quiz/src/App.js
+++ 10-react-quiz/src/App.js
@@ -1,9 +1,14 @@
-import DateCounter from "./DateCounter";
+import Headers from "./Header";
+import Main from "./Main";
 
 export default function App() {
     return (
-        <div>
-            <DateCounter />
+        <div className="app">
+            <Headers />
+            <Main>
+                <p>1/15</p>
+                <p>Question?</p>
+            </Main>
         </div>
     );
 }
