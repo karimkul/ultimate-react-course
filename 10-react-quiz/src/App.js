4422afeda41321a384f0b869f3d5b25822b91cc4
Jami Karimkulov <jami.karimkul@gmail.com>
2025-02-03 10:39:45 -0500

    Add loading questions from a fake API

--- 10-react-quiz/src/App.js
+++ 10-react-quiz/src/App.js
@@ -1,7 +1,39 @@
+import { useEffect, useReducer } from "react";
 import Headers from "./Header";
 import Main from "./Main";
 
+const inisialState = {
+    questions: [],
+    // "loading","error","ready","active","finish"
+    status: "loading"
+};
+
+function reducer(state, action) {
+    switch (action.type) {
+        case "dataRecieved":
+            return {
+                ...state,
+                questions: action.payload,
+                status: "ready"
+            };
+        case "dataFailed":
+            return {
+                ...state,
+                status: "error"
+            };
+        default:
+            throw new Error("Action unknown");
+    }
+}
+
 export default function App() {
+    const [state, dispatch] = useReducer(reducer, inisialState);
+    useEffect(() => {
+        fetch("http://localhost:9000/questions")
+            .then((res) => res.json())
+            .then((data) => dispatch({ type: "dataRecieved", payload: data }))
+            .catch((err) => dispatch({ type: "dataFailed", payload: err }));
+    }, []);
     return (
         <div className="app">
             <Headers />
