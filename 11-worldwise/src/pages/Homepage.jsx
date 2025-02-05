e7636fad5e1307bd3f33c413896e699abaa46f5a
Jami Karimkulov <jami.karimkul@gmail.com>
2025-02-05 13:24:53 -0500

    Implemente main pages and routes

---
+++ 11-worldwise/src/pages/Homepage.jsx
@@ -0,0 +1,5 @@
+function Homepage() {
+    return <div>HomePage</div>;
+}
+
+export default Homepage;
---
+++ 11-worldwise/src/pages/PageNotFound.jsx
@@ -0,0 +1,5 @@
+function PageNotFound() {
+    return <div>Page not found 🙅‍♂️</div>;
+}
+
+export default PageNotFound;
