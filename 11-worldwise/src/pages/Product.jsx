461b5daa3b78d7a190f25460150a85fee0ca901f
Jami Karimkulov <jami.karimkul@gmail.com>
2025-02-07 09:21:18 -0500

    Add using CSS modules

--- 11-worldwise/src/pages/Product.jsx
+++ 11-worldwise/src/pages/Product.jsx
@@ -3,7 +3,6 @@ import PageNav from "../components/PageNav";
 function Product() {
     return (
         <div>
-            {" "}
             <PageNav />
             <h1>Product</h1>
         </div>
