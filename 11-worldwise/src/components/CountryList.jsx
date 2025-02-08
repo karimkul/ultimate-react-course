23a0fda595ce2636ada2b51e558665b16550cd5d
Jami Karimkulov <jami.karimkul@gmail.com>
2025-02-08 11:57:10 -0500

    Implement the countries list

---
+++ 11-worldwise/src/components/CountryList.jsx
@@ -0,0 +1,28 @@
+import CountryItem from "./CountryItem";
+import styles from "./CountryList.module.css";
+import Spinner from "./Spinner";
+import Message from "./Message";
+
+function CountryList({ cities, isLoading }) {
+    if (isLoading) return <Spinner />;
+    if (!cities.length)
+        return (
+            <Message message="Add your first city by clicking on a city on the map" />
+        );
+
+    const countries = cities.reduce((arr, city) => {
+        if (!arr.some((el) => el.country === city.country)) {
+            return [...arr, { country: city.country, emoji: city.emoji }];
+        }
+        return arr;
+    }, []);
+    return (
+        <ul className={styles.countryList}>
+            {countries.map((country) => (
+                <CountryItem country={country} key={country.id} />
+            ))}
+        </ul>
+    );
+}
+
+export default CountryList;
