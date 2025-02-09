656bfb6e8a094311f2e84b38a4d6cd4d6ffc44d1
Jami Karimkulov <jami.karimkul@gmail.com>
2025-02-09 11:26:20 -0500

    Add programmatic navigation with useNavigate

--- 11-worldwise/src/App.jsx
+++ 11-worldwise/src/App.jsx
@@ -10,6 +10,7 @@ import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
+import Form from "./components/Form";
 
 const BASE_URL = "http://localhost:9000";
 
@@ -64,7 +65,7 @@ function App() {
                             />
                         }
                     />
-                    <Route path="from" element={<p>Form</p>} />
+                    <Route path="form" element={<Form />} />
                 </Route>
 
                 <Route path="*" element={<PageNotFound />} />
--- 11-worldwise/src/components/Map.jsx
+++ 11-worldwise/src/components/Map.jsx
@@ -1,7 +1,9 @@
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 
 function Map() {
+    const navigate = useNavigate();
+
     // Use useSearchParams hook to get the search params
     const [searchParams, setSearchParams] = useSearchParams();
 
@@ -10,7 +12,7 @@ function Map() {
     const lng = searchParams.get("lng");
 
     return (
-        <div className={styles.mapContainer}>
+        <div className={styles.mapContainer} onClick={() => navigate("form")}>
             <h1>Map</h1>
             <h1>
                 Position: {lat}, {lng}
