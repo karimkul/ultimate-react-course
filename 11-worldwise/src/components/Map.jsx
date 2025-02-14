40dfbdc78f385851c71d0181dbc1daa08d230a1f
Jami Karimkulov <jami.karimkul@gmail.com>
2025-02-14 11:35:49 -0500

    Interact with the map

--- 11-worldwise/src/components/Map.jsx
+++ 11-worldwise/src/components/Map.jsx
@@ -1,17 +1,37 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import {
+    MapContainer,
+    TileLayer,
+    Marker,
+    Popup,
+    useMap,
+    useMapEvents
+} from "react-leaflet";
 
 import styles from "./Map.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useCities } from "../contexts/CitiesContext";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 function Map() {
     const { cities } = useCities();
     const [mapPosition, setMapPosition] = useState([40, 0]);
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const mapLat = searchParams.get("lat");
+    const mapLng = searchParams.get("lng");
+
+    useEffect(
+        function () {
+            if (mapLat & mapLng) setMapPosition([mapLat, mapLng]);
+        },
+        [mapLat, mapLng]
+    );
 
     return (
         <div className={styles.mapContainer}>
             <MapContainer
                 center={mapPosition}
+                // center={[mapLat, mapLng]}
                 zoom={6}
                 scrollWheelZoom={true}
                 className={styles.map}
@@ -31,9 +51,27 @@ function Map() {
                         </Popup>
                     </Marker>
                 ))}
+                <ChangeCenter position={mapPosition} />
+                <DetectClick />
             </MapContainer>
         </div>
     );
+}
+
+function ChangeCenter({ position }) {
+    const map = useMap();
+    map.setView(position);
+
+    return null;
+}
+function DetectClick() {
+    const navigate = useNavigate();
+
+    useMapEvents({
+        click: (e) => {
+            navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`);
+        }
+    });
 }
 
 export default Map;
