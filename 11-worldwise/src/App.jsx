9537dfa0c0d586a75efcbfad0565f403616ef72e
Jami Karimkulov <jami.karimkul@gmail.com>
2025-02-15 16:13:02 -0500

    Add fake authentication: protecting a route

--- 11-worldwise/src/App.jsx
+++ 11-worldwise/src/App.jsx
@@ -14,6 +14,7 @@ import { CitiesProvider } from "./contexts/CitiesContext";
 import "leaflet/dist/leaflet.css";
 import Map from "./components/Map";
 import AuthProvider from "./contexts/FakeAuthConext";
+import ProtectedRoute from "./pages/ProtectedRoute";
 
 function App() {
     return (
@@ -25,7 +26,14 @@ function App() {
                         <Route path="product" element={<Product />} />
                         <Route path="pricing" element={<Pricing />} />
                         <Route path="login" element={<Login />} />
-                        <Route path="app" element={<AppLayout />}>
+                        <Route
+                            path="app"
+                            element={
+                                <ProtectedRoute>
+                                    <AppLayout />
+                                </ProtectedRoute>
+                            }
+                        >
                             <Route
                                 index
                                 element={<Navigate replace to="cities" />}
