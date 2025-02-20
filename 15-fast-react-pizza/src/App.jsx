0f135747799073045932dd38350082832f4172df
Jami Karimkulov <jami.karimkul@gmail.com>
2025-02-20 11:13:33 -0500

    Set up tailwind CSS

--- 15-fast-react-pizza/src/App.jsx
+++ 15-fast-react-pizza/src/App.jsx
@@ -1,45 +1,45 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import Home from "./ui/Home";
-import Error from "./ui/Error";
-import Menu, { loader as menuLoader } from "./features/menu/Menu";
-import Cart from "./features/cart/Cart";
+import Home from './ui/Home';
+import Error from './ui/Error';
+import Menu, { loader as menuLoader } from './features/menu/Menu';
+import Cart from './features/cart/Cart';
 import CreateOrder, {
-    action as createOrderAction
-} from "./features/order/CreateOrder";
-import Order, { loader as orderLoader } from "./features/order/Order";
-import Applayout from "./ui/Applayout";
+  action as createOrderAction,
+} from './features/order/CreateOrder';
+import Order, { loader as orderLoader } from './features/order/Order';
+import Applayout from './ui/Applayout';
 
 const router = createBrowserRouter([
-    {
-        element: <Applayout />,
+  {
+    element: <Applayout />,
+    errorElement: <Error />,
+    children: [
+      { path: '/', element: <Home /> },
+      {
+        path: '/menu',
+        element: <Menu />,
+        loader: menuLoader,
         errorElement: <Error />,
-        children: [
-            { path: "/", element: <Home /> },
-            {
-                path: "/menu",
-                element: <Menu />,
-                loader: menuLoader,
-                errorElement: <Error />
-            },
-            { path: "/cart", element: <Cart /> },
-            {
-                path: "/order/new",
-                element: <CreateOrder />,
-                action: createOrderAction
-            },
-            {
-                path: "/order/:orderId",
-                element: <Order />,
-                loader: orderLoader,
-                errorElement: <Error />
-            }
-        ]
-    }
+      },
+      { path: '/cart', element: <Cart /> },
+      {
+        path: '/order/new',
+        element: <CreateOrder />,
+        action: createOrderAction,
+      },
+      {
+        path: '/order/:orderId',
+        element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
+      },
+    ],
+  },
 ]);
 
 function App() {
-    return <RouterProvider router={router} />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
