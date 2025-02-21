0d7f740436ad59acfa610a958a508308d2bd0d59
Jami Karimkulov <jami.karimkul@gmail.com>
2025-02-21 13:28:18 -0500

    Add styling the menu

--- 15-fast-react-pizza/src/features/menu/Menu.jsx
+++ 15-fast-react-pizza/src/features/menu/Menu.jsx
@@ -1,21 +1,21 @@
-import { useLoaderData } from "react-router-dom";
-import { getMenu } from "../../services/apiRestaurant";
-import MenuItem from "./MenuItem";
+import { useLoaderData } from 'react-router-dom';
+import { getMenu } from '../../services/apiRestaurant';
+import MenuItem from './MenuItem';
 
 function Menu() {
-    const menu = useLoaderData();
-    return (
-        <ul>
-            {menu.map((pizza) => (
-                <MenuItem pizza={pizza} key={pizza.id} />
-            ))}
-        </ul>
-    );
+  const menu = useLoaderData();
+  return (
+    <ul className="devide-y devide-stone-200 px-2">
+      {menu.map((pizza) => (
+        <MenuItem pizza={pizza} key={pizza.id} />
+      ))}
+    </ul>
+  );
 }
 
 export async function loader() {
-    const menu = await getMenu();
-    return menu;
+  const menu = await getMenu();
+  return menu;
 }
 
 export default Menu;
