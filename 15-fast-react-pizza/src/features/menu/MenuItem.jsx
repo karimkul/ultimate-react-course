7a336fb18ab37b57a6b0133b06ff379c607f53f8
Jami Karimkulov <jami.karimkul@gmail.com>
2025-02-26 10:02:12 -0500

    Update cart quantities

--- 15-fast-react-pizza/src/features/menu/MenuItem.jsx
+++ 15-fast-react-pizza/src/features/menu/MenuItem.jsx
@@ -7,6 +7,7 @@ import {
   getTotalCartQuantity,
 } from '../cart/cartSlice';
 import DeleteItem from '../cart/DeleteItem';
+import UpdateItemQuantity from '../cart/UpdateItemQuantity';
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
@@ -47,7 +48,15 @@ function MenuItem({ pizza }) {
             </p>
           )}
 
-          {isInCart && <DeleteItem pizzaId={id} />}
+          {isInCart && (
+            <div className="flex items-center gap-3 sm:gap-8">
+              <UpdateItemQuantity
+                pizzaId={id}
+                currentQuantity={currentQuantity}
+              />
+              <DeleteItem pizzaId={id} />
+            </div>
+          )}
           {!soldOut && !isInCart && (
             <Button type="small" className="text-sm" onClick={handleAddToCart}>
               Add to cart
