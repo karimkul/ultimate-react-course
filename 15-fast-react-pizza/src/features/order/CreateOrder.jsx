28f2bacb1dc571f9762b60163870dd0ff36547ab
Jami Karimkulov <jami.karimkul@gmail.com>
2025-02-26 11:29:44 -0500

    Add redux thunks with createAsyncThunk

--- 15-fast-react-pizza/src/features/order/CreateOrder.jsx
+++ 15-fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -1,12 +1,13 @@
 import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
 import Button from '../../ui/Button';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { clearCart, getCart, getTotalCartPrice } from '../cart/cartSlice';
 import EmptyCart from '../cart/EmptyCart';
 import store from '../../store';
 import { formatCurrency } from '../../utils/helpers';
 import { useState } from 'react';
+import { fetchAddress } from '../user/userSlice';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -52,10 +53,15 @@ function CreateOrder() {
   const priorityPrice = withPriority ? totalCartPrice * 0.2 : 0;
   const totalPrice = totalCartPrice + priorityPrice;
 
+  const dispatch = useDispatch();
+
   if (!cart.length) return <EmptyCart />;
+
   return (
     <div className="px-6 py-4">
       <h2 className="mb-8 text-xl font-semibold">Ready to order? Lets go!</h2>
+
+      <button onClick={() => dispatch(fetchAddress())}>Get position</button>
 
       {/* <Form method="POST" action="/order/new"> */}
       <Form method="POST">
