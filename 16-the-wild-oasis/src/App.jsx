d4f769338f44e88b249f97333de03eb086b63243
Jami Karimkulov <jami.karimkul@gmail.com>
2025-02-27 22:53:39 -0500

    Build more reusable styled components

--- 16-the-wild-oasis/src/App.jsx
+++ 16-the-wild-oasis/src/App.jsx
@@ -3,9 +3,10 @@ import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
 import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 const StyledApp = styled.div`
-    background-color: red;
+    /* background-color: red; */
     padding: 20px;
 `;
 function App() {
@@ -13,14 +14,43 @@ function App() {
         <>
             <GlobalStyles />
             <StyledApp>
-                <Heading as="h1">The Wild Oasis</Heading>
-                <Button onClick={() => alert("Check in ✅")}>Check in</Button>
-                <Heading as="h2">Check in and out</Heading>
+                <Row>
+                    <Row type="horizontal">
+                        <Heading as="h1">The Wild Oasis</Heading>
 
-                <Button onClick={() => alert("Check out ✅")}>Check out</Button>
+                        <div>
+                            <Heading as="h2">Check in and out</Heading>
+                            <Button
+                                variation="primary"
+                                size="medium"
+                                onClick={() => alert("Check in ✅")}
+                            >
+                                Check in
+                            </Button>
 
-                <Heading as="h3">Form</Heading>
-                <Input type="number" placeholder="Number of guests" />
+                            <Button
+                                variation="secondary"
+                                size="small"
+                                onClick={() => alert("Check out ✅")}
+                            >
+                                Check out
+                            </Button>
+                        </div>
+                    </Row>
+                    <Row>
+                        <Heading as="h3">Form</Heading>
+                        <form>
+                            <Input
+                                type="number"
+                                placeholder="Number of guests"
+                            />
+                            <Input
+                                type="number"
+                                placeholder="Number of guests"
+                            />
+                        </form>
+                    </Row>
+                </Row>
             </StyledApp>
         </>
     );
