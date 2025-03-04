8da749a32f2500fb164b79ddd17a973e3fe1ff2d
Jami Karimkulov <jami.karimkul@gmail.com>
2025-03-04 14:50:03 -0500

    Upload images to supabase

--- 16-the-wild-oasis/src/services/supabase.js
+++ 16-the-wild-oasis/src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://sokqzffmbrlkbokdzjoa.supabase.co";
-const supabaseKey =
+export const supabaseUrl = "https://sokqzffmbrlkbokdzjoa.supabase.co";
+export const supabaseKey =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNva3F6ZmZtYnJsa2Jva2R6am9hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA4NTg0MTAsImV4cCI6MjA1NjQzNDQxMH0.VAgApfxjUMClFXxSSPph6LdTgKtO0CYny8GQ8Arxqgk";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
