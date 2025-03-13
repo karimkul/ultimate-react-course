e6c659f149066293464a1a905f70a5c466c61f42
Jami Karimkulov <jami.karimkul@gmail.com>
2025-03-13 15:02:16 -0400

    Convert the modal to a compound component

--- 16-the-wild-oasis/src/features/cabins/AddCabins.jsx
+++ 16-the-wild-oasis/src/features/cabins/AddCabins.jsx
@@ -2,24 +2,45 @@ import { useState } from "react";
 import Button from "../../ui/Button";
 import CreateCabinForm from "./CreateCabinForm";
 import Modal from "../../ui/Modal";
+import CabinTable from "./CabinTable";
 
 function AddCabins() {
-    const [isOpenModal, setIsOpenModal] = useState(false);
-
     return (
-        <div>
-            <Button onClick={() => setIsOpenModal((show) => !show)}>
-                Add new cabin
-            </Button>
-            {isOpenModal && (
-                <Modal onClose={() => setIsOpenModal(false)}>
-                    <CreateCabinForm
-                        onCloseModal={() => setIsOpenModal(false)}
-                    />
-                </Modal>
-            )}
-        </div>
+        <Modal>
+            <Modal.Open opens="cabin-form">
+                <Button>Add new cabin</Button>
+            </Modal.Open>
+            <Modal.Window name="cabin-form">
+                <CreateCabinForm />
+            </Modal.Window>
+
+            <Modal.Open opens="table">
+                <Button>Show table</Button>
+            </Modal.Open>
+            <Modal.Window name="table">
+                <CabinTable />
+            </Modal.Window>
+        </Modal>
     );
 }
 
 export default AddCabins;
+
+// function AddCabins() {
+//     const [isOpenModal, setIsOpenModal] = useState(false);
+
+//     return (
+//         <div>
+//             <Button onClick={() => setIsOpenModal((show) => !show)}>
+//                 Add new cabin
+//             </Button>
+//             {isOpenModal && (
+//                 <Modal onClose={() => setIsOpenModal(false)}>
+//                     <CreateCabinForm
+//                         onCloseModal={() => setIsOpenModal(false)}
+//                     />
+//                 </Modal>
+//             )}
+//         </div>
+//     );
+// }
