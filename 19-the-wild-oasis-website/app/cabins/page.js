e4d08d5a683c3b6e7336d53fa7e08a372b5e5871
Jami Karimkulov <jami.karimkul@gmail.com>
2025-03-20 13:11:37 -0400

    Add interactivity with client components

--- 19-the-wild-oasis-website/app/cabins/page.js
+++ 19-the-wild-oasis-website/app/cabins/page.js
@@ -1,7 +1,9 @@
+import Counter from "../components/Counter";
+
 export default async function Page() {
     const res = await fetch("https://jsonplaceholder.typicode.com/users");
     const data = await res.json();
-    console.log(data);
+    // console.log(data);
     return (
         <div>
             <h1>Cabin Page</h1>
@@ -10,6 +12,7 @@ export default async function Page() {
                     <li key={user.id}>{user.name}</li>
                 ))}
             </ul>
+            <Counter users={data} />
         </div>
     );
 }
