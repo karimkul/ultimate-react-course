65e8deee44f31a88a03be336be1e924eeddac44f
Jami Karimkulov <jami.karimkul@gmail.com>
2025-03-22 13:18:30 -0400

    Build the home page

--- 19-the-wild-oasis-website/app/layout.js
+++ 19-the-wild-oasis-website/app/layout.js
@@ -8,8 +8,6 @@ const josefin = Josefin_Sans({
     display: "swap"
 });
 
-console.log(josefin);
-
 import "@/app/_styles/globals.css";
 import Header from "./_components/Header";
 export const metadata = {
@@ -26,10 +24,10 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <body
-                className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
+                className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col `}
             >
                 <Header />
-                <div className="flex-1 px-8 py-12">
+                <div className="flex-1 px-8 py-12 relative">
                     <main className="max-w-7xl mx-auto">{children}</main>
                 </div>
             </body>
