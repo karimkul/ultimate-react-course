0cc6ca4d6e2009f742fc4658a4bf82ccf228e19b
Jami Karimkulov <jami.karimkul@gmail.com>
2025-03-26 13:43:14 -0400

    Experiment with caching and ISR

--- 19-the-wild-oasis-website/app/_components/CabinList.js
+++ 19-the-wild-oasis-website/app/_components/CabinList.js
@@ -1,7 +1,11 @@
+import { unstable_noStore as noStore } from "next/cache";
+
 import CabinCard from "./CabinCard";
 import { getCabins } from "../_lib/data-service";
 
 export default async function CabinList() {
+    // noStore();
+
     const cabins = await getCabins();
 
     if (cabins.length === 0) return null;
