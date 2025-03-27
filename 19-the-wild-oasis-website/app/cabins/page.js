bf802ab28718eedcbc88ba8a52b45308d5a34f1c
Jami Karimkulov <jami.karimkul@gmail.com>
2025-03-27 10:38:26 -0400

    Share state between client and server: the URL

--- 19-the-wild-oasis-website/app/cabins/page.js
+++ 19-the-wild-oasis-website/app/cabins/page.js
@@ -2,6 +2,7 @@ import Counter from "@/app/_components/Counter";
 import CabinList from "../_components/CabinList";
 import { Suspense } from "react";
 import Spinner from "../_components/Spinner";
+import Filter from "../_components/Filter";
 
 export const metadata = {
     title: `Cabins`
@@ -10,7 +11,11 @@ export const metadata = {
 export const revalidate = 0;
 // export const revalidate = 15;
 
-export default function Page() {
+export default function Page({ searchParams }) {
+    console.log(searchParams);
+
+    const filter = searchParams?.capacity ?? "all";
+
     return (
         <div>
             <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -24,8 +29,11 @@ export default function Page() {
                 nature&apos;s beauty in your own little home away from home. The
                 perfect spot for a peaceful, calm vacation. Welcome to paradise.
             </p>
-            <Suspense fallback={<Spinner />}>
-                <CabinList />
+            <div className="flex justify-end mb-8">
+                <Filter />
+            </div>
+            <Suspense fallback={<Spinner />} key={filter}>
+                <CabinList filter={filter} />
             </Suspense>
         </div>
     );
